5e1b021119e5a459aed01b2476d6b92bd4b28631
Epigene <augusts.bautra@gmail.com>
2015-02-18 14:58:58 +0200

    50% with request

--- lib/sacps/auth/citadele.rb
+++ lib/sacps/auth/citadele.rb
@@ -1,70 +1,22 @@
 require 'sacps/auth/citadele/helper'
 require 'sacps/auth/citadele/notification'
 
-require 'xmldsig'
-
 module SacPS
   module Auth
     module Citadele
-      mattr_accessor :public_key
-      mattr_accessor :private_key
-      mattr_accessor :identifier
-      mattr_accessor :service_url
-      mattr_accessor :return_url
 
-      def self.get_public_key
-        cert = self.public_key
-        OpenSSL::X509::Certificate.new(cert.gsub(/  /, '')).public_key
-      end
+      mattr_accessor :return_url, :identifier, :service_url, :public_key, :private_key
 
-      def self.get_private_key
-        private_key = self.private_key
-        OpenSSL::PKey::RSA.new(private_key.gsub(/  /, ''))
-      end
+      SacPS::Auth::Citadele.service_url = "https://online.citadele.lv/amai/start.htm"
 
-      def self.get_X509_subject
-        self.get_private_key.X509SubjectName
-      end
+      # def self.notification post
+      #   Notification.new post
+      # end
 
-      def self.notification post 
-        Notification.new post 
-      end
-
-      def self.helper options={}
-        Helper.new options
-      end
-
-      def sign_xml xml
-        unsigned_document = Xmldsig::SignedDocument.new xml 
-        # signed_xml = unsigned_document.sign self.class.parent.get_private_key
-
-        signed_xml = unsigned_document.sign do |data|
-          self.class.parent.get_private_key.sign(OpenSSL::Digest::SHA1.new, data)
-        end
-
-        is_valid_xml = validate_xml signed_xml
-        return signed_xml, is_valid_xml
-      end
-
-      def validate_xml signed_xml
-        signed_document = Xmldsig::SignedDocument.new signed_xml
-        signed_document.validate(self.class.parent.get_public_key)
-      end
-
-      protected
-
-        def get_public_key_as_string
-          cert = self.public_key
-          public_key = OpenSSL::X509::Certificate.new(cert.gsub(/  /, '')).public_key.to_s
-
-          normalized_public_key = ""
-          public_key.split(/\r?\n/).each {|line| normalized_public_key << line }
-
-          normalized_public_key = normalized_public_key.gsub("-----BEGIN PUBLIC KEY-----","").gsub("-----END PUBLIC KEY-----","")
-
-          return normalized_public_key
-        end
+      # def self.helper options={}
+      #   Helper.new options
+      # end
 
     end
   end
-end  
+end
--- lib/sacps/auth/citadele/helper.rb
+++ lib/sacps/auth/citadele/helper.rb
@@ -1,117 +1,65 @@
-require "builder"
-require "nokogiri"
-
 module SacPS
   module Auth
     module Citadele
       class Helper
-        attr_reader :fields
 
         include SacPS::Auth::Common
         include SacPS::Auth::Citadele
 
-        def initialize options={}
-          @options = {}
-          @fields = {}
-          
-          @options['from'] = SacPS::Auth::Citadele.identifier
-          @options['timestamp'] = Time.now.strftime("%Y%m%d%H%M%S%3N")
-          @options['request'] = request
-          @options['requestUID'] = generate_random_string 30
-          @options['return_url'] = SacPS::Auth::Citadele.return_url
-          @options['language'] = options[:language] || "LV"
-
-          @doc = build_xml
-          @doc, @built_valid_xml = sign_xml @doc
-          form_signature_ds!
-          canonicalize!
+        def initialize
+          @account = SacPS::Auth::Citadele.account
+          @return_url = SacPS::Auth::Citadele.return_url
+          @xml = build_request_xml
         end
 
-        def valid?
-          @built_valid_xml
+        # def sign(unsigned_string)
+        #   rsa         = OpenSSL::PKey::RSA.new(File.read(CPS.config.rsa_cert_path), CPS.config.cert_pass )
+        #   signed_hash = rsa.sign(OpenSSL::Digest::SHA1.new, unsigned_string)
+        #   Base64.encode64(signed_hash)
+        # end
+
+        def build_request_xml
+          timestamp = Time.now.strftime("%Y%m%d%H%M%S%3N") # "20030905175959000"
+          unique_id = SecureRandom.uuid # "7387bf5b-fa27-4fdd-add6-a6bfb2599f77"
+          request = "AUTHREQ"
+          version = "3.0"
+          language = "LV"
+          signature = sign( [timestamp, @account, request, unique_id, version, language, @return_url].join )
+
+          xml = <<-XML
+<?xml version="1.0" encoding="UTF-8" ?>
+<FIDAVISTA xmlns="http://ivis.eps.gov.lv/XMLSchemas/100017/fidavista/v1-1"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://ivis.eps.gov.lv/XMLSchemas/100017/fidavista/v1-1
+http://ivis.eps.gov.lv/XMLSchemas/100017/fidavista/v1-1/fidavista.xsd">
+  <Header>
+    <Timestamp>#{timestamp}</Timestamp>
+    <From>#{@account}</From>
+    <Extension>
+      <Amai xmlns="http://online.citadele.lv/XMLSchemas/amai/"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://online.citadele.lv/XMLSchemas/amai/
+      http://online.citadele.lv/XMLSchemas/amai/amai.xsd">
+        <Request>#{request}</Request>
+        <RequestUID>#{unique_id}</RequestUID>
+        <Version>#{version}</Version>
+        <Language>#{language}</Language>
+        <ReturnURL>#{@return_url}</ReturnURL>
+        <SignatureData>
+          <Signature xmlns="http://www.w3.org/2000/09/xmldsig#">
+            #{signature}
+          </Signature>
+        </SignatureData>
+      </Amai>
+    </Extension>
+  </Header>
+</FIDAVISTA>
+          XML
+
+          return xml.chomp
         end
 
-        def xml
-          @doc
-        end
-
-        def build_xml
-          doc = ""
-
-          xml = Builder::XmlMarkup.new(indent: 1, target: doc)
-          xml.instruct!
-          xml.FIDAVISTA("xmlns" => "http://ivis.eps.gov.lv/XMLSchemas/100017/fidavista/v1-1", "xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance", "xsi:schemaLocation" => "http://ivis.eps.gov.lv/XMLSchemas/100017/fidavista/v1-1 http://ivis.eps.gov.lv/XMLSchemas/100017/fidavista/v1-1/fidavista.xsd") do
-            xml.Header do
-              xml.Timestamp @options['timestamp']
-              xml.From @options['from']
-              xml.Extension do
-                xml.Amai("xmlns" => "http://online.citadele.lv/XMLSchemas/amai/", "xsi:schemaLocation" => "http://online.citadele.lv/XMLSchemas/amai/ http://online.citadele.lv/XMLSchemas/amai/amai.xsd") do
-                  xml.Request @options['request']
-                  xml.RequestUID @options['requestUID']
-                  xml.Version version
-                  xml.Language @options['language']
-                  xml.ReturnURL @options['return_url']
-                  xml.SignatureData do
-                    build_signature xml
-                  end
-                end  
-              end
-            end
-          end
-
-          return doc
-        end
-
-        def build_signature xml
-          xml.Signature("xmlns" =>"http://www.w3.org/2000/09/xmldsig#") do
-            xml.SignedInfo do
-              xml.CanonicalizationMethod "Algorithm"=>"http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
-              xml.SignatureMethod "Algorithm" => "http://www.w3.org/2000/09/xmldsig#rsa-sha1"
-              xml.Reference "URI" => "" do
-                
-                xml.Transforms do
-                  xml.Transform("Algorithm"=>"http://www.w3.org/2000/09/xmldsig#enveloped-signature")
-                end
-
-                xml.DigestMethod("Algorithm"=>"http://www.w3.org/2000/09/xmldsig#sha1")
-                xml.DigestValue
-
-              end
-            end
-            xml.SignatureValue
-
-            xml.KeyInfo do
-              xml.X509Data do
-                xml.X509Certificate get_public_key_as_string 
-              end
-            end
-          end
-        end
-
-        def form_signature_ds!
-          ["Signature", "SignedInfo", "CanonicalizationMethod", "Reference", "Transforms", "Transform", "DigestMethod", "DigestValue", "KeyInfo", "X509Data", "X509Certificate"].each do |signature_data| 
-            @doc = @doc.gsub(signature_data, "ds:#{signature_data}")
-          end
-          @doc = @doc.gsub "ds:SignatureData", "SignatureData"
-          @doc = @doc.gsub "ds:ds:", "ds:"
-        end
-
-        def canonicalize!
-          doc = Nokogiri::XML(@doc, nil, nil, Nokogiri::XML::ParseOptions::NOBLANKS | Nokogiri::XML::ParseOptions::NOCDATA | Nokogiri::XML::ParseOptions::STRICT).canonicalize
-          xml = '<?xml version="1.0" encoding="UTF-8"?>' + "\n"
-          doc.split(/\r?\n/).each {|line| xml << line}
-          @doc = xml
-        end
-
-        def version
-          "3.0"
-        end
-
-        def request
-          "AUTHREQ"
-        end
-
-      end
+      end # -- Class Helper ends
     end
   end
 end
